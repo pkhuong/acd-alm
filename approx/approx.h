149906bbbfd0cfa3cc0e7626d15010efe9b35d53
Paul Khuong <pvk@pvk.ca>
2014-01-08 20:15:04 -0500

    nvars in prototype

--- approx/approx.h
+++ approx/approx.h
@@ -18,7 +18,7 @@ sparse_matrix_t approx_matrix(approx_t);
 size_t approx_nrhs(approx_t);
 double * approx_rhs(approx_t);
 double * approx_weight(approx_t);
-size_t approx_nvar(approx_t);
+size_t approx_nvars(approx_t);
 double * approx_linear(approx_t);
 double * approx_lower(approx_t);
 double * approx_upper(approx_t);

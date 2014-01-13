28e600c1747b63fa4e2565eb33765db728ea9158
Paul Khuong <pvk@pvk.ca>
2014-01-13 03:43:03 -0500

    Less racy thread pool

--- thread_pool/thread_pool.c
+++ thread_pool/thread_pool.c
@@ -5,16 +5,21 @@
 #include <string.h>
 #include <strings.h>
 
-struct job;
+struct job
+{
+        size_t barrier_waiting_for;
+        size_t id;
+        size_t limit;
+        size_t increment;
+
+        thread_pool_function function;
+        void * info;
+};
 
 struct thread_pool
 {
         pthread_t * threads;
         unsigned nthreads;
-        unsigned nactive;
-        pthread_mutex_t lock;
-        pthread_cond_t new_job_queue;
-        pthread_cond_t job_done_queue;
         struct job * job;
         unsigned job_sequence;
         unsigned worker_id_counter;
@@ -31,10 +36,6 @@ thread_pool_t thread_pool_init(unsigned nthreads)
         thread_pool_t pool = calloc(1, sizeof(struct thread_pool));
         pool->threads = calloc(allocated_threads, sizeof(pthread_t));
         pool->nthreads = allocated_threads;
-        pthread_mutex_init(&pool->lock, NULL);
-        pthread_cond_init(&pool->new_job_queue, NULL);
-        pthread_cond_init(&pool->job_done_queue, NULL);
-        pool->worker_id_counter = 1;
 
         for (unsigned i = 0; i < allocated_threads; i++) {
                 int ret = pthread_create(pool->threads+i, NULL,
@@ -45,7 +46,18 @@ thread_pool_t thread_pool_init(unsigned nthreads)
         return pool;
 }
 
-static void set_job(thread_pool_t, struct job *);
+static void set_job(thread_pool_t pool, struct job * job)
+{
+        assert(pool->job == NULL);
+        assert(job != NULL);
+        if (job != (struct job*)-1ul)
+                assert(job->barrier_waiting_for
+                       == (pool->nthreads+1));
+
+        __sync_fetch_and_add(&pool->job_sequence, 1);
+        pool->job = job;
+        __sync_synchronize();
+}
 
 void thread_pool_free(thread_pool_t pool)
 {
@@ -59,21 +71,8 @@ void thread_pool_free(thread_pool_t pool)
                 assert(0 == ret);
         }
         free(pool->threads);
-        pthread_mutex_destroy(&pool->lock);
-        pthread_cond_destroy(&pool->new_job_queue);
-        pthread_cond_destroy(&pool->job_done_queue);
         memset(pool, 0, sizeof(struct thread_pool));
         free(pool);
-}
-
-static void set_job(thread_pool_t pool, struct job * job)
-{
-        assert(pool->job == NULL);
-        assert(pool->nactive == 0);
-
-        __sync_fetch_and_add(&pool->job_sequence, 1);
-        pool->job = job;
-        __sync_synchronize();
 }
 
 static struct job * get_job(thread_pool_t pool)
@@ -81,7 +80,6 @@ static struct job * get_job(thread_pool_t pool)
         struct job * job = NULL;
         while (NULL == (job = pool->job))
                 __asm__("":::"memory");
-        __sync_fetch_and_add(&pool->nactive, 1);
 
         return job;
 }
@@ -89,10 +87,11 @@ static struct job * get_job(thread_pool_t pool)
 static void release_job(thread_pool_t pool, struct job * job, int master)
 {
         (void)master;
-        assert(pool->nactive > 0);
+        assert(job->barrier_waiting_for > 0);
         assert(pool->job == job);
         unsigned sequence = pool->job_sequence;
-        unsigned nactive = __sync_sub_and_fetch(&pool->nactive, 1);
+        unsigned nactive
+                = __sync_sub_and_fetch(&job->barrier_waiting_for, 1);
         if (0 == nactive) {
                 pool->job = NULL;
                 __sync_fetch_and_add(&pool->job_sequence, 1);
@@ -102,16 +101,6 @@ static void release_job(thread_pool_t pool, struct job * job, int master)
         while (pool->job_sequence == sequence)
                 __asm__("":::"memory");
 }
-
-struct job
-{
-        size_t id;
-        size_t limit;
-        size_t increment;
-
-        thread_pool_function function;
-        void * info;
-};
 
 static void do_job(struct job * job, unsigned self)
 {
@@ -133,14 +122,8 @@ static void do_job(struct job * job, unsigned self)
 static void * worker(void * thunk)
 {
         thread_pool_t pool = thunk;
-        unsigned worker_id;
-        {
-                int ret = pthread_mutex_lock(&pool->lock);
-                assert(0 == ret);
-                worker_id = pool->worker_id_counter++;
-                ret = pthread_mutex_unlock(&pool->lock);
-                assert(0 == ret);
-        }
+        unsigned worker_id
+                = __sync_add_and_fetch(&pool->worker_id_counter, 1);
 
         while (1) {
                 struct job * job = get_job(pool);
@@ -153,11 +136,12 @@ static void * worker(void * thunk)
         return NULL;
 }
 
-static void init_job(struct job * job,
+static void init_job(struct job * job, thread_pool_t pool,
                      size_t begin, size_t end, size_t granularity,
                      thread_pool_function function, void * info)
 {
         if (granularity == 0) granularity = 1;
+        job->barrier_waiting_for = pool->nthreads+1;
         job->id = begin;
         job->limit = end;
         job->increment = granularity;
@@ -168,11 +152,7 @@ static void init_job(struct job * job,
 static void execute_job(thread_pool_t pool, struct job * job)
 {
         assert(NULL == pool->job);
-        assert(0 == pool->nactive);
-        __sync_fetch_and_add(&pool->job_sequence, 1);
-        pool->job = job;
-        __sync_fetch_and_add(&pool->nactive, 1);
-
+        set_job(pool, job);
         do_job(job, 0);
         release_job(pool, job, 1);
 }
@@ -199,7 +179,7 @@ void thread_pool_for(thread_pool_t pool,
         }
 
         struct job job;
-        init_job(&job, from, end, 
+        init_job(&job, pool, from, end, 
                  ideal_granularity(end-from, granularity,
                                    pool->nthreads+1),
                  function, info);

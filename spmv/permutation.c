ddb333bdb34c9f779bd069a5cc3d65deb794e403
Paul Khuong <pvk@pvk.ca>
2014-01-15 17:03:52 -0500

    Compute permutations

--- spmv/permutation.c
+++ spmv/permutation.c
@@ -19,14 +19,57 @@ void sparse_permutation_identity(sparse_permutation_t * permutation,
                 permutation->idx[i] = permutation->ridx[i] = i;
 }
 
+struct count_pair
+{
+        uint32_t count;
+        uint32_t initial_index;
+};
+
+static int compare_count_pair(const void * x, const void * y)
+{
+        const struct count_pair * p1 = x,
+                * p2 = y;
+        if (p1->count < p2->count) return -1;
+        if (p1->count > p2->count) return 1;
+
+        if (p1->initial_index < p2->initial_index) return -1;
+        if (p1->initial_index > p2->initial_index) return 1;
+        return 0;
+}
+
 void sparse_permutation_init(sparse_permutation_t * destination,
                              const sparse_matrix_t * matrix,
                              int row)
 {
-        (void)destination;
-        (void)matrix;
-        (void)row;
-        assert(0);
+        assert(NULL == destination->idx);
+        assert(NULL == destination->ridx);
+        assert(0 == destination->n);
+
+        size_t n = row?matrix->nrows:matrix->ncolumns;
+        struct count_pair * pairs = calloc(n, sizeof(struct count_pair));
+
+        for (size_t i = 0; i < n; i++)
+                pairs[i].initial_index = i;
+        {
+                const uint32_t * src = row?matrix->rows:matrix->columns;
+                size_t nnz = matrix->nnz;
+                for (size_t i = 0; i < nnz; i++)
+                        pairs[src[i]].count++;
+        }
+
+        qsort(pairs, n, sizeof(struct count_pair), compare_count_pair);
+
+        destination->idx = calloc(n, sizeof(uint32_t));
+        destination->ridx = calloc(n, sizeof(uint32_t));
+        destination->n = n;
+
+        for (size_t i = 0; i < n; i++) {
+                uint32_t src = pairs[i].initial_index;
+                destination->idx[i] = src;
+                destination->ridx[src] = i;
+        }
+
+        free(pairs);
 }
 
 void sparse_permutation_clear(sparse_permutation_t * permutation)
